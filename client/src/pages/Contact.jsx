a115b4b2d7e11cc50d6172ea82d82710c35d10b6
Om-Dwivedi2 <dwivediom2005@gmail.com>
2026-06-17 20:56:37 +0530

    feat: enhance contact page with new input fields and styling for better user experience

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import telephone from "../assets/contactTelephone.png";
 import headphone from "../assets/contactHeadphone.png";
 import { FiUser } from "react-icons/fi";
+import { FaPaperPlane } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+import { LuPencil } from "react-icons/lu";
+import { LuNewspaper } from "react-icons/lu";
 const Contact = () => {
   return (
     <>
-      <main className="px-40 bg-(--color-base-100)">
+      <main className="px-50 bg-(--color-base-100) space-y-5 pb-10">
         <section className="flex items-center w-full">
           <div className="space-y-3 w-[50%] ">
             <div className="text-xs font-semibold text-(--color-primary) bg-(--color-primary-subtle) rounded-xl w-fit px-2 py-1 ">
@@ -23,30 +27,68 @@ const Contact = () => {
           </div>
         </section>
 
-        <section className="flex">
-          <div>
-            <h2 className="text-2xl">Send Us a Message</h2>
+        <section className="grid grid-cols-[2fr_1fr] gap-10  bg-white shadow rounded p-10 ">
+          <div className="space-y-5">
+            <h2 className="text-3xl font-semibold">Send Us a Message</h2>
             <p>Fill out the form below and we'll get back to you soon.</p>
-            <div>
-              <div className="border border-(--color-secondary) flex gap-2 items-center p-2 rounded-md">
+            <div className="flex gap-3">
+              <div className="border border-(--color-secondary) flex gap-2 items-center p-2 rounded-md w-[50%]">
                 <FiUser className="text-(--color-secondary)" />
                 <input
                   type="text"
                   name="name"
                   id="contactName"
                   placeholder="Enter your name"
+                  className="w-full outline-none "
+                />
+              </div>
+              <div className="border border-(--color-secondary) flex gap-2 items-center p-2 rounded-md w-[50%]">
+                <MdOutlineMail className="text-(--color-secondary)" />
+                <input
+                  type="text"
+                  name="name"
+                  id="contactName"
+                  placeholder="Enter your Email"
+                  className="w-full outline-none "
                 />
               </div>
             </div>
+            <div className="border border-(--color-secondary) flex gap-2 items-center p-2 rounded-md w-[100%]">
+              <LuNewspaper className="text-(--color-secondary)" />
+              <input
+                type="text"
+                name="name"
+                id="contactName"
+                placeholder="Enter your subject"
+                className="w-full outline-none "
+              />
+            </div>
+            <div className="border border-(--color-secondary) flex gap-2 items-top p-2 rounded-md w-[100%]">
+              <LuPencil className="text-(--color-secondary)" />
+              <textarea
+                type="text"
+                rows="10"
+                name="name"
+                id="contactName"
+                placeholder="Enter your Message"
+                className="w-full outline-none "
+              />
+            </div>
+
+            <button className="rounded bg-(--color-primary) text-white text-lg font-semibold py-2 px-3 flex gap-2 items-center active:scale-95">
+              Send Message <FaPaperPlane />
+            </button>
           </div>
 
-          <div className="bg-(--color-base-100)">
-            <h2 className="text-2xl font-semibold text-(--color-primary)">We're Here to Help!</h2>
+          <div className="bg-(--color-base-100) p-10 space-y-3 rounded">
+            <h2 className="text-2xl font-semibold text-(--color-primary)">
+              We're Here to Help!
+            </h2>
             <p>
               Our team is dedicated to providing you with the best support. Let
               us know how we can assist you.
             </p>
-            <img src={headphone} alt="" className="w-100"/>
+            <img src={headphone} alt="" className="w-100" />
           </div>
         </section>
       </main>
